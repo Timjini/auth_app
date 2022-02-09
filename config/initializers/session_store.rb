ae677c1406f626ad9da172f859621b5c59ae71b7
Timjini <hatim.jini@gmail.com>
2022-02-09 14:17:55 +0300

    sessions_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "your-production-app-here.com"
+  Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "https://shrouded-hollows-01022.herokuapp.com/"
   else
     Rails.application.config.session_store :cookie_store, key: '_auth_app'
   end
