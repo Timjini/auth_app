98b4f50d627a81585f10f1013e5c101892af394c
Timjini <hatim.jini@gmail.com>
2022-02-09 15:00:26 +0300

    CORS false Update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3001"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    resource "http://localhost:3002", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: false
   end
 
   allow do
     origins "https://objective-kilby-bd1d9f.netlify.app"
-    resource "https://shrouded-hollows-01022.herokuapp.com/", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    resource "https://shrouded-hollows-01022.herokuapp.com/", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: false
   end
 end
